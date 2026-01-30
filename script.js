2b7020975d25249d5d9878a24f08ca4b77c3648f
Skylar Jacobs <skylar.jacobs@du.edu>
2026-01-30 14:36:23 -0700

    Fixed spacing issues and resized all images of the dog to match each other

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // will delete later, this is to test stuff
 const dog = document.querySelector('.dog');
 
-const dogStates = ['idle', 'playful', 'playing', 'sleeping', 'hungry', 'eating', 'sad'];
-    let currentIndex = 0;
+const dogStates = ['idle', 'playful', 'playing', 'sleeping', 'hungry', 'eating', 'sad'];   
+let currentIndex = 0;
 
 dog.addEventListener('click', function() {
     currentIndex = (currentIndex + 1) % dogStates.length;
